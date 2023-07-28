9e242b199bba77bebbe1b8c9f7dac285cb41bb43
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-07-29 00:30:46 +0700

    Initial Commit

---
+++ NavigationStackExample/NavigationStackExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackExampleApp.swift
+//  NavigationStackExample
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
